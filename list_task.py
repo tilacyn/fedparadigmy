013d38190e4acd981dd840d9c003f75a81f2b739
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-09-16 21:01:38 +0300

    Add files via upload

--- list_task.py
+++ list_task.py
@@ -1,25 +1,27 @@
 def remove_adjacent(lst):
-    i=0;
-    while i<len(lst): 
-        if lst[i]==lst[i-1]:
-                   lst.pop(i)
-        i=i+1
-    return lst
-
+    l=[]
+    l.append(lst[0])
+    for i in range(1,len(lst)):
+        if lst[i]!=l[-1]:
+            l.append(lst[i])
+    return l
 
 def linear_merge(lst1, lst2):
     lst=[]
-    for i in range(len(lst1)+len(lst2)):
-        if len(lst1)==0:
-            lst.extend(lst2)
-            break
-        if len(lst2)==0:
-            lst.extend(lst1)
-            break
-        if lst1[0]<=lst2[0]:
-            lst3.append(lst1[0])
-            lst1.pop(0)
+    a1=0; a2=0
+    while a1<len(lst1)and a2<len(lst2):
+        
+        if lst1[a1]<=lst2[a2]:
+            lst.append(lst1[a1])
+            a1=a1+1
         else:
-            lst.append(lst2[0])
-            lst2.pop(0)
+            lst.append(lst2[a2])
+            a2=a2+1
+            
+    if a1>=len(lst1):
+            lst.extend(lst2[a2:])
+    if a2>+len(lst2):
+            lst.extend(lst1[a1:])
+            
     return lst
+
